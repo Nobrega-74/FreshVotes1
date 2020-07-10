177fa241612abef75e16d3c1db6357c50be20323
mcarm <mcarm@LAPTOP-V29OHAA0>
2020-07-10 19:44:32 +0100

    initial check-in

---
+++ FreshVotes1/src/main/java/com/example/demo/FreshVotes1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVotes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVotes1Application.class, args);
+	}
+
+}
